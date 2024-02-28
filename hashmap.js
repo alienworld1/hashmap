623b8024831b7c7e43eebaaa75fbbe7fb5d4608d
alienworld1 <astronomyguy0030@gmail.com>
2024-02-28 21:07:03 +0530

    Add set method

--- hashmap.js
+++ hashmap.js
@@ -1,12 +1,16 @@
+import LinkedList from './linkedlist.js';
+
 const loadFactor = 0.75;
 
 export default class HashMap {
 
     #size;
     #capacity;
+    #buckets;
 
     hash(key) {
         let hashCode = 0;
+        key = String(key);
            
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
@@ -18,7 +22,32 @@ export default class HashMap {
       }
     
     constructor() {
-        this.#size = 16;
+        this.#size = 32;
         this.#capacity = 0;
+        this.#buckets = [];
+    }
+
+    set(key, value) {
+        const keyHash = this.hash(key);
+
+        if (!this.#buckets[keyHash]) {
+            this.#buckets[keyHash] = new LinkedList({
+                key,
+                value,
+            });
+        } 
+
+        else {
+            this.#buckets[keyHash].append({
+                key,
+                value,
+            });
+        }
+
+        this.#capacity += 1;
+        if (this.#capacity > this.#size * loadFactor) {
+            this.resize(); 
+        }
+
     }
 }
---
+++ linkedlist.js
@@ -0,0 +1,148 @@
+function Node(value=null, next=null) {
+    return {
+        value,
+        next,
+    }
+}
+
+export default class LinkedList {
+
+    #head;
+    #tail;
+
+    constructor(headValue) {
+        const head = Node(headValue);
+        this.#head = head;
+        this.#tail = head;
+    }
+
+    get head() {
+        return this.#head;
+    }
+
+    get tail() {
+        return this.#tail;
+    }
+
+    append(value) {
+        const node = Node(value);
+        this.#tail.next = node;
+        this.#tail = node;
+    }
+
+    prepend(value) {
+        const node = Node(value);
+        node.next = this.#head;
+        this.#head = node;
+    }
+
+    get size() {
+        let size = 1;
+        let node = this.head;
+
+        while (node.next) {
+            size++;
+            node = node.next;
+        }
+
+        return size;
+    }
+
+    at(index) {
+        let node = this.head;
+
+        for (let i = 0; i < index; i++) {
+            node = node.next;
+            if (!node) {
+                return null;
+            }
+        }
+
+        return node;
+    }
+
+    pop() {
+        const size = this.size;
+        if (size === 1) {
+            throw new Error('The list contains only one node.');
+        }
+
+        this.#tail = this.at(size - 2);
+        this.#tail.next = null;
+    }
+
+    contains(value) {
+        let node = this.head;
+
+        while(node) {
+            if (node.value === value) {
+                return true;
+            }
+
+            node = node.next;
+        }
+
+        return false;
+    }
+
+    find(value) {
+        let node = this.head;
+        for (let i = 0; node; i++) {
+            if (node.value === value) {
+                return i;
+            }
+
+            node = node.next;
+        }
+
+        return null;
+    }
+
+    toString() {
+        let result = '';
+        let node = this.head;
+
+        while (node) {
+            result += `${node.value} -> `;
+            node = node.next;
+        }
+
+        result += 'null';
+        return result;
+    }
+
+    insertAt(value, index) {
+        if (index === 0) {
+            this.prepend(value);
+            return;    
+        }
+
+        const node = Node(value);
+        const current = this.at(index - 1);
+
+        if (!current) {
+            throw new Error('This index is not accessible by this linked list.');
+        }
+
+        const next = current.next;
+
+        current.next = node;
+        node.next = next;
+    }
+
+    removeAt(index) {
+        if (index === 0) {
+            this.#head = this.#head.next;
+            return;
+        }
+
+        const current = this.at(index - 1);
+        if (!current.next) {
+            throw new Error('The index does not exist in this linked list.');
+        }
+
+        current.next = current.next.next;
+
+    }
+
+}
