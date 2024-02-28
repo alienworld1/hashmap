5f28546bd4ab0f21f107778db24a4a61558196e9
alienworld1 <astronomyguy0030@gmail.com>
2024-02-28 20:22:23 +0530

    Add capacity and size

--- hashmap.js
+++ hashmap.js
@@ -11,6 +11,7 @@ export default class HashMap {
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
           hashCode = primeNumber * hashCode + key.charCodeAt(i);
+          hashCode %= this.#size;
         }
      
         return hashCode;
