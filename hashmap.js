4e4813684d96616e1635f04bb7a2ad4af18f301b
alienworld1 <astronomyguy0030@gmail.com>
2024-02-28 20:15:52 +0530

    Add a hash function

---
+++ hashmap.js
@@ -0,0 +1,23 @@
+const loadFactor = 0.75;
+
+export default class HashMap {
+
+    #size;
+    #capacity;
+
+    hash(key) {
+        let hashCode = 0;
+           
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+     
+        return hashCode;
+      }
+    
+    constructor() {
+        this.#size = 16;
+        this.#capacity = 0;
+    }
+}
